1024c3ce2b817d00e3fdee0cfcc54bef3e04ec83
mohdfarhad <MohammadFarhadBaig@LAPTOP-3PEEJHUQ>
2025-02-13 12:03:54 +0530

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using AzureSqlMvcApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AzureSqlMvcApp.Data
+{
+    public class ApplicationDbContext : DbContext   
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
